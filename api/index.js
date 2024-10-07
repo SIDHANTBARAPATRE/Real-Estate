642ca82d05b5c80b5ec5fcb6f281bda843c29db5
Sidhant Barapatre <sidhantbarapatre12@gmail.com>
2024-10-07 12:46:26 +0530

    creating api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js"; // router changed---> userRouter
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(() => {
     console.log("connected to MongoDB");
@@ -14,3 +15,6 @@ const app = express();
 app.listen(3000, () => {
     console.log("server is running on port 3000")
 });
+
+app.use("/api/user", userRouter); /// api/user- is not a folder path
+
