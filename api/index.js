9602b9e28e9aea4757d31d19fee6937c64af1b3a
Sidhant Barapatre <sidhantbarapatre12@gmail.com>
2024-10-08 23:47:53 +0530

    Creating a middleware and a function to handle Middleware

--- api/index.js
+++ api/index.js
@@ -23,3 +23,15 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter); /// api/user- is not a folder path
 app.use("/api/auth", authRouter);
 
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message 
+
+     'Internal Server Error';
+     return  res.status(statusCode).json({
+        success : false,
+        statusCode,
+        message,
+     });
+});
+
