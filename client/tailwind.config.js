f68f3d36c58aeed906f1644915bb52366433105d
Sidhant Barapatre <sidhantbarapatre12@gmail.com>
2025-04-02 19:44:56 +0530

    changes made

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,14 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+import lineClamp from '@tailwindcss/line-clamp';
+
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/line-clamp'),
-    
-  ],
-}
+  plugins: [lineClamp],
+};
