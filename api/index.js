c09170aeb6df35fd7132a1f3f29c6258573c8f2f
Sidhant Barapatre <sidhantbarapatre12@gmail.com>
2025-04-02 19:33:53 +0530

    changes made

--- api/index.js
+++ api/index.js
@@ -6,8 +6,9 @@ import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
+
 dotenv.config();
-console.log(process.env.MONGO);
+const __dirname = path.resolve();
 
 mongoose
   .connect(process.env.MONGO)
@@ -18,29 +19,25 @@ mongoose
     console.log(err);
   });
 
-  const __dirname = path.resolve();
-
 const app = express();
 
 app.use(express.json());
-
 app.use(cookieParser());
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
-});
+// Serve static frontend files
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
 
+// API routes
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
-
-app.use(express.static(path.join(__dirname, '/client/dist')));
-
+// Catch-all for frontend routes
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
+});
 
+// Global error handler
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
@@ -49,4 +46,8 @@ app.use((err, req, res, next) => {
     statusCode,
     message,
   });
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
 });
